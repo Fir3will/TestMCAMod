750e13adf088b30f925f347834eb8da4caa91d79
Fir3will <owner@Owners-MacBook-Pro.local>
2015-02-24 18:05:53 -0500

    Fixed some incorrect information
    Fixed the MODID and also the mcmod.info file. Also moved the assets
    folder

--- src/main/java/main/testmca/TestMCA.java
+++ src/main/java/main/testmca/TestMCA.java
@@ -33,7 +33,7 @@ public class TestMCA
 
 	public static class Info
 	{
-		public static final String MODID = "mobtutorial";
+		public static final String MODID = "testmca";
 		public static final String VERSION = "1.0";
 		public static final String CLIENT_CLASS = "main.testmca.MCAClient";
 		public static final String COMMON_CLASS = "main.testmca.MCACommon";
